26bcc094ad5989389bbcc05c3562f28472502008
Alexandre Machado <alexandre@machado.cc>
2022-08-30 02:21:00 +0000

    add distinct

--- src/text-search.test.js
+++ src/text-search.test.js
@@ -3,12 +3,16 @@ test('iterate and find text', () => {
     const data = [
         {message: "Merge branch 'main' into feat/jira-10-integration-tests"},
         {message: "jira-15: some changes"},
+        {message: "jira-15: revert changes"},
         {message: "fix: another changes"}
     ];
     let matches = data
         .map(s => s.message.match(/([A-Z0-9]+)-[0-9]+/gi))
+        //remove null
         .filter(m => m)
         .flat()
+        // distinct
+        .filter((v, i, a) => a.indexOf(v) === i)
         .map(m => m.toUpperCase());
 
     expect(matches).toEqual([

99142998b1eb064d81d6dcb52db8ed2665e3976e
Alexandre Machado <alexandre@machado.cc>
2022-09-08 21:43:43 +0000

    teste sort, distinct and fix upperCase

--- src/text-search.test.js
+++ src/text-search.test.js
@@ -3,20 +3,24 @@ test('iterate and find text', () => {
     const data = [
         {message: "Merge branch 'main' into feat/jira-10-integration-tests"},
         {message: "jira-15: some changes"},
-        {message: "jira-15: revert changes"},
-        {message: "fix: another changes"}
+        {message: "JIRA-15: revert changes"},
+        {message: "fix: another changes"},
+        {message: "Jira-11: more changes"},
     ];
     let matches = data
         .map(s => s.message.match(/([A-Z0-9]+)-[0-9]+/gi))
         //remove null
         .filter(m => m)
         .flat()
+        .map(m => m.toUpperCase())
         // distinct
         .filter((v, i, a) => a.indexOf(v) === i)
-        .map(m => m.toUpperCase());
+        .sort()
+    ;
 
     expect(matches).toEqual([
         "JIRA-10",
-        "JIRA-15"
+        "JIRA-11",
+        "JIRA-15",
     ]);
 });
